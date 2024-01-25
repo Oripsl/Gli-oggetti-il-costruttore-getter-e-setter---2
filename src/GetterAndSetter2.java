d56be777f6ecad7ef95848a1ab3a72eeac749fa5
Basile Bartolomeo <basilebartolomeo01@gmail.com>
2024-01-25 13:45:00 +0100

    moved class auto in main

--- src/GetterAndSetter2.java
+++ src/GetterAndSetter2.java
@@ -21,3 +21,52 @@ public class GetterAndSetter2 {
         System.out.println("Modello: " + auto1.getModello());
     }
 }
+
+class Auto {
+    private int cilindrata;
+
+    private String targa;
+
+    private String marca;
+
+    private String modello;
+
+    public Auto(int cilindrata,String targa, String marca, String modello) {
+        this.cilindrata = cilindrata;
+        this.targa = targa;
+        this.marca = marca;
+        this.modello = modello;
+    }
+
+    // Getter setter cilindrata
+    public int getCilindrata() {
+        return cilindrata;
+    }
+    public void setCilindrata(int cilindrata){
+        this.cilindrata = cilindrata;
+    }
+
+    // Getter setter targa
+    public String getTarga() {
+        return targa;
+    }
+    public void setTarga(String targa) {
+        this.targa = targa;
+    }
+
+    // Getter setter marca
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    // Getter setter modello
+    public String getModello() {
+        return modello;
+    }
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+}
